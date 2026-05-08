23b1e430541d784a4056f1c024e207126ec95c24
musheaven-rgb <musheaven@gmail.com>
2026-05-08 13:00:29 +0800

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '20px' }}>
+      <h1>优朗口才</h1>
+
+      <p>专业少儿口才与主持培训机构</p>
+
+      <img
+        src="/images/logo.png"
+        alt="logo"
+        style={{ width: '220px', marginTop: '20px' }}
+      />
+
+      <h2 style={{ marginTop: '40px' }}>联系我们</h2>
+
+      <p>电话 / 微信：18934856011</p>
+      <p>地址：广西北流市莲石路57-3号</p>
+
+      <h2 style={{ marginTop: '40px' }}>扫码咨询</h2>
+
+      <img
+        src="/images/qrcode.jpg"
+        alt="二维码"
+        style={{ width: '220px' }}
+      />
+    </div>
+  )
+}
+
+export default App
